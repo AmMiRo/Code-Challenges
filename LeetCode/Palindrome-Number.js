f6284546ca5162834c9ac067bb5e65e371880889
Amos Rose <amosroseacct@gmail.com>
2020-10-12 14:51:53 -0600

    palindrome number challenge added

---
+++ LeetCode/Palindrome-Number.js
@@ -0,0 +1,18 @@
+// Description
+
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+
+
+// Solution
+
+const isPalindrome = function(x) {
+    const reverse = (x + "").split("").reverse().join("");
+    if (x < 0) {
+        return(false);
+    } else if (x == reverse) {
+        return(true);
+    } else {
+      return(false);  
+    };
+};
