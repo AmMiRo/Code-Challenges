9972dac148091c424c07625d12c59c8c4614abd0
Amos Rose <amosroseacct@gmail.com>
2020-10-17 15:20:08 -0600

    Remove-Element added

---
+++ LeetCode/Remove-Element.js
@@ -0,0 +1,24 @@
+// Description
+
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+
+
+// Solution
+
+const removeElement = function(nums, val) {
+    
+    let i = 0;
+    for(j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j];
+            i++;
+        };
+    };
+    console.log(nums);
+    return i
+};
