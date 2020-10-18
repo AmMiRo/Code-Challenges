0169775ba837eaf0b58c9c74a8af343bc9541c98
Amos Rose <amosroseacct@gmail.com>
2020-10-17 19:05:45 -0600

    Implement-strStr() added

---
+++ LeetCode/Implement-strStr().js
@@ -0,0 +1,21 @@
+// Description
+
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+// Solution
+
+const strStr = function(haystack, needle) { 
+    if (needle === "") {
+        return 0;
+    };
+    for (i = 0; i < haystack.length; i++) {
+        if (haystack.substr(i, needle.length) === needle) {
+            return i;
+        };
+    };
+    return -1;
+};
