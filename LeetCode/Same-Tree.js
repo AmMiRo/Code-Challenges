70334bea342f8d6a67b2bbd086cf6a796be97e3e
Amos Rose <amosroseacct@gmail.com>
2020-10-22 14:24:13 -0600

    Same Tree added

---
+++ LeetCode/Same-Tree.js
@@ -0,0 +1,21 @@
+// Description
+
+// Given two binary trees, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+
+
+// Solution
+
+const isSameTree = function(p, q) {
+    if (p === null || q === null) {
+        return p === q;
+    }
+    
+    if (p.val !== q.val) {
+        return false;
+    }
+    
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
