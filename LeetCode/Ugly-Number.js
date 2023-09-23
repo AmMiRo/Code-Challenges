b5224eff33465624f3b083fbd2e7aa9446520c80
Amos Rose <amos.m.rose@gmail.com>
2023-09-23 13:44:30 -0600

    valid anagram

--- LeetCode/Ugly-Number.js
+++ LeetCode/Ugly-Number.js
@@ -4,8 +4,6 @@
 
 // Given an integer n, return true if n is an ugly number.
 
- 
-
 // Example 1:
 
 // Input: n = 6
@@ -24,33 +22,31 @@
 // Output: false
 // Explanation: 14 is not ugly since it includes the prime factor 7.
 
-
-
 // Solution
 
 // First Pass
 function isUgly(n) {
-    if (n === 0) return false;
-    
-    while (n % 5 === 0) n = n/5;
-    
-    while (n % 3 === 0) n = n/3;
-    
-    while (n % 2 === 0) n = n/2;
-    
-    return n === 1;
-};
+  if (n === 0) return false;
+
+  while (n % 5 === 0) n = n / 5;
+
+  while (n % 3 === 0) n = n / 3;
+
+  while (n % 2 === 0) n = n / 2;
+
+  return n === 1;
+}
 
 // Second Pass
 function isUgly(n) {
-    if (n === 0) return false;
-    
-    while (n > 1) {
-        if (n % 5 === 0) n/=5;
-        else if (n % 3 === 0) n/=3;
-        else if (n % 2 === 0) n/=2;
-        else return false;
-    }
-    if (n === 1) return true;
-    return false
-};
+  if (n === 0) return false;
+
+  while (n > 1) {
+    if (n % 5 === 0) n /= 5;
+    else if (n % 3 === 0) n /= 3;
+    else if (n % 2 === 0) n /= 2;
+    else return false;
+  }
+  if (n === 1) return true;
+  return false;
+}
--- LeetCode/Valid-Anagram.js
+++ LeetCode/Valid-Anagram.js
@@ -4,8 +4,6 @@
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
-
 // Example 1:
 
 // Input: s = "anagram", t = "nagaram"
@@ -16,22 +14,45 @@
 // Input: s = "rat", t = "car"
 // Output: false
 
-
-
 // Solution
 
 // First Pass
 function isAnagram(s, t) {
-    if (s.length !== t.length) return false;
+  if (s.length !== t.length) return false;
 
-    const sArr = Array.from(s).sort();
-    const tArr = Array.from(t).sort();
+  const sArr = Array.from(s).sort();
+  const tArr = Array.from(t).sort();
 
-    for (let i = 0; i < sArr.length; i++) {
-        if (sArr[i] !== tArr[i]) {
-            return false;
-        }
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i] !== tArr[i]) {
+      return false;
     }
+  }
 
-    return true;
-};
+  return true;
+}
+
+// Second Pass (Better)
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+
+  const charObj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (charObj[s[i]]) {
+      charObj[s[i]]++;
+    } else {
+      charObj[s[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (charObj[t[i]] === undefined || charObj[t[i]] === 0) {
+      return false;
+    } else {
+      charObj[t[i]]--;
+    }
+  }
+
+  return true;
+}
